ca2c89689eb0beda5ed375fc9b2a02fc2668aa75
Wilson <67292323+whl-usc@users.noreply.github.com>
2023-10-23 16:51:57 -0700

    Update functions, index, and css

--- xlrit_home/functions.js
+++ xlrit_home/functions.js
@@ -1,9 +1,11 @@
-/* Open the sidenav */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("SiteNav").style.display = "block";
+  document.getElementById("SiteNav").style.width = "150px";
+  document.getElementById("main").style.marginLeft = "150px";
 }
 
-/* Close/hide the sidenav */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("SiteNav").style.display = "none";
+  document.getElementById("SiteNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
