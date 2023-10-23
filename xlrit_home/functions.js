8f546befb3ce40a77a9396e15e1fef860fae4184
Wilson <67292323+whl-usc@users.noreply.github.com>
2023-10-23 14:27:53 -0700

    Update function and index file.

--- xlrit_home/functions.js
+++ xlrit_home/functions.js
@@ -1,9 +1,9 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("SiteNav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("SiteNav").style.width = "0";
 }
