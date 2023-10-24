9a7ebee14338d872534e3ba16985f2bbc7120fb2
Wilson <67292323+whl-usc@users.noreply.github.com>
2023-10-24 16:08:35 -0700

    Updated index.html, webstyle.css, functions.js for navigation.

--- xlrit_home/functions.js
+++ xlrit_home/functions.js
@@ -1,11 +1,20 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Set the width of the side navigation and left margin of the page content */
 function openNav() {
-  document.getElementById("SiteNav").style.width = "150px";
-  document.getElementById("main").style.marginLeft = "150px";
+  document.getElementById("SiteNav").style.width = "190px";
+  document.getElementById("main").style.marginLeft = "190px";
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("SiteNav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+}
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+function toggleDropdown(button) {
+  button.classList.toggle("active");
+  var dropdownContent = button.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
 }
