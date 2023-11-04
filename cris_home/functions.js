5a5e8a6202bd634db80e60c7a83b333e559a0631
Wilson <wlee9829@gmail.com>
2023-11-03 18:48:15 -0700

    Updated JS, HTML, and CSS files to reflect changes in style.

--- cris_home/functions.js
+++ cris_home/functions.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation and left margin of the page content */
 function openNav() {
-  document.getElementById("SiteNav").style.width = "190px";
-  document.getElementById("main").style.marginLeft = "190px";
+  document.getElementById("SiteNav").style.width = "145px";
+  document.getElementById("main").style.marginLeft = "145px";
 }
 function closeNav() {
   document.getElementById("SiteNav").style.width = "0";
